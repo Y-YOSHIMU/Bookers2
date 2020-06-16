23dbaa6c476a93fd76832f938d03903f67677051
Y-YOSHIMU <mcu7.oldtr.uk@gmail.com>
2020-06-16 15:36:29 +0900

    [Add]Book_new_show

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_025649) do
+ActiveRecord::Schema.define(version: 2020_06_16_063344) do
 
   create_table "books", force: :cascade do |t|
-    t.integer "title"
     t.text "body"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
