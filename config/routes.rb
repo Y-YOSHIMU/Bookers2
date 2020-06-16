5cc44358ac048317e44eb41d0279e514360d38ef
Y-YOSHIMU <mcu7.oldtr.uk@gmail.com>
2020-06-16 17:09:41 +0900

    [Update]Book_index_show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   	registrations: 'users/registrations'
   }
   root to: 'home#top'
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
   resources :books, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
