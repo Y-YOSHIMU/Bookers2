25d9d6f840f70520a38f0cce61ed4be0cefd0dba
Y-YOSHIMU <mcu7.oldtr.uk@gmail.com>
2020-06-17 23:31:12 +0900

    [Add]他の投稿者が編集画面に繊維できないようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -31,5 +32,12 @@ class UsersController < ApplicationController
   private
   def user_params
   	params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def correct_user
+    user = User.find(params[:id])
+    if current_user != user
+      redirect_to root_path
+    end
   end
 end
