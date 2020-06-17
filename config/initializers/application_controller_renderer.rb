c580ae0063bc054756292b4e68a7543088852265
Y-YOSHIMU <mcu7.oldtr.uk@gmail.com>
2020-06-17 11:48:53 +0900

    [Add]Profile-image

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c8586bcf6160eb6a02eb1264d1916a747366c2f66f244bef754bd326c9670180ce416f923d81040809e48974b2bae7a9c8314f8f75e9e298a0617aec3a0afe58'
